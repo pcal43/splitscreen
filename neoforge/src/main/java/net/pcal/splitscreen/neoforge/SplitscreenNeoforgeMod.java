ee84d832d84275a43e5596e92dab0c5083e51e08
Patrick Calahan <github@pcal.net>
2026-01-26 15:53:30 -0800

    workaround NeoForge mod startup threading

--- neoforge/src/main/java/net/pcal/splitscreen/neoforge/SplitscreenNeoforgeMod.java
+++ neoforge/src/main/java/net/pcal/splitscreen/neoforge/SplitscreenNeoforgeMod.java
@@ -1,7 +1,10 @@
 package net.pcal.splitscreen.neoforge;
 
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,13 +17,25 @@ public class SplitscreenNeoforgeMod {
 
     private static final Logger LOGGER = LogManager.getLogger(SplitscreenNeoforgeMod.class);
     private static final Path CONFIG_DIR_PATH = Path.of("config");
+    private static boolean initialized = false;
 
     public SplitscreenNeoforgeMod(IEventBus modBus) {
-        // NOTE: we can't do this in onClientSetup because neoforge does
-        // some early window initialization before that, and that initialization
-        // will break our mixins if the config isn't set up.  It seems
-        // fairly safe to do it here.
-        mod().onModInitialize(CONFIG_DIR_PATH);
-        LOGGER.info("[Splitscreen] Mod initialized");
+        NeoForge.EVENT_BUS.addListener(this::onClientTick);
+    }
+
+    /**
+     * With NeoForge, mod initialization is not on the main thread (I guess),
+     * which causes Minecraft to complain when we try to reposition the
+     * window in a thread other than main.  This is sort of a dumb workaround
+     * for that.  It seems to not be an issue in later versions (e.g., 1.21.11)
+     * where we didn't have to do this.
+     */
+    @SubscribeEvent
+    public void onClientTick(ClientTickEvent.Pre event) {
+        if (!initialized) {
+            initialized = true;
+            mod().onModInitialize(CONFIG_DIR_PATH);
+            LOGGER.info("[Splitscreen] Mod initialized");
+        }
     }
 }
